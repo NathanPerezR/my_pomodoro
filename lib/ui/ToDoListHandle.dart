08426a52a9e71b1bf143960f4b4b2fee47502ee8
Nathan Perez <nathanrp62@gmail.com>
2023-07-16 10:12:21 -0500

    dialog added, list now populates

--- lib/ui/ToDoListHandle.dart
+++ lib/ui/ToDoListHandle.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
 class ToDoListHandle extends StatefulWidget {
   const ToDoListHandle({Key? key}) : super(key: key);
@@ -9,53 +8,113 @@ class ToDoListHandle extends StatefulWidget {
 }
 
 class _ToDoListHandleState extends State<ToDoListHandle> {
-  
-  @override
-  Widget build(BuildContext context) {
-      final List<Map<String, String>> listOfColumns = [
-    {"Name": "AAAAAA", "Number": "1", "State": "Yes"},
-    {"Name": "BBBBBB", "Number": "2", "State": "no"},
-    {"Name": "CCCCCC", "Number": "3", "State": "Yes"}
-  ];
-  
-    return DefaultTextStyle(
-      style: const TextStyle(
-        color: Colors.white,
-        fontSize: 20.0,
-      ),
-      child: Stack(
-          children: [
-            Container(
-              height: 350,
-              width: 300,     
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(20.0),
-              ),
-              child:  DataTable(
-        columns: [
-          DataColumn(label: Text('Patch')),
-          DataColumn(label: Text('Version')),
-          DataColumn(label: Text('Ready')),
-        ],
-        rows:
-            listOfColumns // Loops through dataColumnText, each iteration assigning the value to element
-                .map(
-                  ((element) => DataRow(
-                        cells: <DataCell>[
-                          DataCell(Text(element["Name"]!)), //Extracting from Map element the value
-                          DataCell(Text(element["Number"]!)),
-                          DataCell(Text(element["State"]!)),
-                        ],
-                      )),
-                )
-                .toList(),
-      ),
-              ), 
-            
+  final List<Map<String, String>> listOfColumns = [];
+
+  final TextStyle _textStyle =  const TextStyle(color: Colors.white);
+
+  void addListItem(String taskName) {
+    setState(() {
+      listOfColumns.add({"Name": taskName});
+    });
+  }
+
+  Future<void> _showAddTaskDialog() async {
+    String newTaskName = '';
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add Task'),
+          content: TextField(
+            onChanged: (value) {
+              newTaskName = value;
+            },
+            decoration: const InputDecoration(
+              hintText: 'Enter task name',
+            ),
+            autofocus: true,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('Cancel', style: TextStyle(color: Colors.black)),
+            ),
+            TextButton(
+              onPressed: () {
+                addListItem(newTaskName);
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('Add', style: TextStyle(color: Colors.black)),
+            ),
           ],
-        ),
+        );
+      },
     );
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 350,
+          width: 300,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 0, 0, 0),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Theme(
+            data: Theme.of(context).copyWith(dividerColor: Colors.white),
+            child: DataTable(
+              columns: [
+                DataColumn(label: Text('Task Name', style: _textStyle)),
+                DataColumn(label: Text('', style: _textStyle)),
+              ],
+              rows: listOfColumns
+                  .map(
+                    (element) => DataRow(
+                      cells: <DataCell>[
+                        DataCell(
+                          Container(
+                            alignment: Alignment.centerLeft,
+                            child: Text(element["Name"]!, style: _textStyle),
+                          ),
+                        ),
+                        DataCell(
+                          Container(
+                            alignment: Alignment.centerRight,
+                            child: IconButton(
+                              color: Colors.white,
+                              icon: const Icon(Icons.remove),
+                              onPressed: () {
+                                setState(() {
+                                  listOfColumns.remove(element);
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 302,
+          right: 24,
+          child: FloatingActionButton(
+            onPressed: _showAddTaskDialog, // Call the dialog function
+            mini: true,
+            backgroundColor: Colors.black,
+            child: const Icon(Icons.add),
+          ),
+        ),
+      ],
+    );
+  }
 }
